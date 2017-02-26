c4525f82d3f38a1678b5004af103e21808138941
kangli <likang914@hotmail.com>
2017-02-26 15:52:21 -0500

    add org backup code app_tmp.js

---
+++ app_tmp.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+var express = require('express')
+  , app = express()
+  , bodyparser = require('body-parser')
+  , fs = require('fs')
+  , ini = require('ini')
+  , mysql = require('mysql')
+
+// load mysql config
+var config = ini.parse( fs.readFileSync('./config.ini', 'utf-8') )
+  , pool = mysql.createPool(config.database)
+
+// set directives
+app.use( express.static('static') )
+app.use( bodyparser.json() )
+app.use( bodyparser.urlencoded({
+  extended:true
+}))
+
+/* define routes */
+
+// returns a list of environments to select from
+.get('/clientip/', function(req,res){
+  pool.getConnection( function(j, connection){
+    connection.query("SELECT count(DISTINCT(clientip)) as IpCount FROM `request`", function(e,r){
+      if( e || r.length == 0 ){
+        console.log(e)
+      }
+      res.send(r)
+      connection.release()
+    })
+   })
+})
+
+// listen on port 3000
+app.listen(3000)
