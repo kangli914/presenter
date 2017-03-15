33f8fa594e244c3588ccbc8caeb22f496d29e359
kangli <likang914@hotmail.com>
2017-03-15 10:20:45 -0400

    report

--- static/main.js
+++ static/main.js
@@ -1,5 +1,5 @@
-    var ctx = document.getElementById("c").getContext("2d");
-    var data = {
+var ctx = document.getElementById("c").getContext("2d");
+var data = {
       labels: ["January", "February", "March", "April", "May", "June", "July"],
       datasets: [{
         label: "My First dataset",
@@ -20,6 +20,23 @@
         pointHighlightStroke: "rgba(151,187,205,1)",
         data: [28, 48, 40, 19, 86, 27, 90]
       }]
-    };
-    //var Chart = require('chart.js');
-    var MyNewChart = new Chart(ctx).Line(data);
+	}; 
+	
+var data2 = {
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
+    datasets: [{
+      label: 'apples',
+      backgroundColor: "rgba(153,255,51,0.4)",
+      borderColor: "rgba(153,255,51,1)",
+      data: [12, 19, 3, 17, 28, 24, 7]
+    }, {
+      label: 'oranges',
+      backgroundColor: "rgba(255,153,0,0.4)",
+      borderColor: "rgba(255,153,0,1)",
+      data: [30, 29, 5, 5, 20, 3, 10]
+    }]
+  }
+	
+var Chart = require('chart.js')
+//var MyNewChart = new Chart(ctx).Line(data);
+//var MyNewChart = new Chart(ctx).Radar(data2);
