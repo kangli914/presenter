90add167cc175593884419133b9719a7f57c6465
kangli <likang914@hotmail.com>
2017-03-15 16:33:29 -0400

    add python and bash utility to process log

---
+++ utility/FusionReactorProcessor/file_opt.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+tmpdir="tmp/fr-logrotator-plugin"
+rm $tmpdir/request.log
+
+for f in logs/toscana/FusionReactor/2016*/*.zip
+do
+	echo "Processing $f file..."
+	unzip $f fr-logrotator-plugin/request.log -d tmp/
+	
+	srcfile="request.log"
+	fileword=$(head -n1 $tmpdir/$srcfile | awk '{print $1, $2}');
+	date=$(echo $fileword | awk '{print $1}')
+	hr=$(echo $fileword | cut -f2 -d" " | cut -f1 -d":")
+	min=$(echo $fileword | cut -f2 -d" " | cut -f2 -d":")
+	trgfile=$date"_"$hr$min
+
+	# Everything after first dot is considered as extention
+	ext=$(echo $srcfile | sed 's/.*\.\(.*\)/.\1/g')
+	prefix=$(echo $srcfile | cut -f1 -d".")
+	trgfile=$prefix"_"$trgfile$ext
+	cp "$tmpdir/$srcfile" "$tmpdir/$trgfile"
+	
+	rm $tmpdir/$srcfile
+done
---
+++ utility/mount_windows_presenter.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+### INIT INFO
+# Provides:          windows_desktop
+# Required-Start:    $local_fs $network
+# Required-Stop:     $local_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: windows_desktop
+# Description:       mounts windows share 'C:\debianvm\presenter' to debian /home/kangli/presenter
+### END INIT INFO
+
+
+
+# fix the mount issue when windows rebooted
+
+sudo umount /home/kangli/presenter > /dev/null 2>&1
+sudo mount.cifs //10.0.2.2/debianvm/presenter /home/kangli/presenter -o user=kangli,password=Winter2017,rw,uid=1000,gid=1000
